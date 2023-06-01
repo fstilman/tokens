298c14b270bc339c681718c68965bd20384938cd
Federico Stilman <fstilman@gmail.com>
2023-05-31 22:55:57 -0300

    Changed "entity" to "token".

--- tokens.el
+++ tokens.el
@@ -10,39 +10,40 @@
 ;;; Commentary:
 
 ;; tokens.el provides functions for matching well-known elements on
-;; the current line and saving them to the kill ring, for easy
-;; yanking.  Tokens or elements are easily customizable through
-;; regular expressions.  Included are regular expressions for matching
-;; numbers, emails and IP addresses.
+;; the current line and saving them to the kill ring for easy
+;; yanking.  This elements, that we call "tokens" are easily
+;; customizable through standard regular expressions.
+;; Included are regular expressions for matching numbers, emails
+;; and IP addresses.
 
 ;;; Code:
 
-(defgroup entities nil
-  "Settings for entities matching on buffers."
-  :group 'entities)
+(defgroup tokens nil
+  "Search for a customizable token and save it to the kill ring."
+  :group 'tokens)
 
-(defcustom entities-regex
-  '((number . "[0-9]+")
+(defcustom tokens-regex
+  '((number . "\\b[0-9]+\\b")
     (email . "\\b[A-Za-z0-9]+@[A-Za-z0-9]+\\.[A-Za-z]\\{2,\\}\\b")
     (ip . "\\b\\(25[0-5]\\|2[0-4][0-9]\\|[01]?[0-9][0-9]?\\)\\.\\(25[0-5]\\|2[0-4][0-9]\\|[01]?[0-9][0-9]?\\)\\.\\(25[0-5]\\|2[0-4][0-9]\\|[01]?[0-9][0-9]?\\)\\.\\(25[0-5]\\|2[0-4][0-9]\\|[01]?[0-9][0-9]?\\)\\b"))
   "List of supported tokens.
 Each token is defined by a dotted pair where the car is a symbol and
 the cdr is a regular expression for matching this kind of token."
   :type '(alist :key-type symbol :value-type regexp)
-  :group 'entities)
+  :group 'tokens)
 
-(defun entities-regex-for-entity (entity-type)
-  "Return a regex for matching entities of type ENTITY-TYPE."
-  (cdr (assoc entity-type entities-regex)))
+(defun tokens-regex-for-token (token-type)
+  "Return a regex for matching tokens of type TOKEN-TYPE."
+  (cdr (assoc token-type tokens-regex)))
 
-(defun entities-kill-ring-save (entity-type &optional match-number)
-  "Search the current line for the Nth MATCH-NUMBER entity.
-The search is done for ENTITY-TYPE tokens.  When a match is found
+(defun tokens-kill-ring-save (token-type &optional match-number)
+  "Search the current line for the Nth MATCH-NUMBER token.
+The search is done for TOKEN-TYPE tokens.  When a match is found
 it is saved to the kill ring.  If MATCH-NUMBER is ommited it defaults
 to 1."
   (interactive
    (list (intern (completing-read
-                  "Enter entity type: "
+                  "Enter token type: "
                   '(number email)
                   nil
                   t))
@@ -51,7 +52,7 @@ to 1."
     (beginning-of-line)
     (let ((match-number (or match-number 1)))
       (when (search-forward-regexp
-             (entities-regex-for-entity entity-type)
+             (tokens-regex-for-token token-type)
              (line-end-position)
              t
              match-number)
