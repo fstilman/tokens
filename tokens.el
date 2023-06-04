e01cbad548960b71bfa6b7b85a6f4c47cd66c359
Federico Stilman <fstilman@gmail.com>
2023-06-04 12:02:36 -0300

    Code refactoring

--- tokens.el
+++ tokens.el
@@ -28,6 +28,7 @@
     (date . "\\b\\([0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}\\)\\|\\([0-9]\\{2\\}/[0-9]\\{1,2\\}/[0-9]\\{4\\}\\)\\b")
     (number . "\\b[0-9]+\\b"))
   "List of supported tokens.
+
 Each token is defined by a dotted pair where the CAR is a symbol
 representing the token type and the CDR is a regular expression
 that should match this type of token."
@@ -35,28 +36,21 @@ that should match this type of token."
   :group 'tokens)
 
 (defcustom tokens-highlight-match t
-  "Whether matched token is highlighted. Defaults to true."
+  "Whether matched token is highlighted.  Defaults to true."
   :type 'boolean
   :group 'tokens)
 
 (defcustom tokens-verbose-match t
-  "Whether matched token is shown on echo area. Defaults to true."
+  "Whether matched token is shown on echo area.  Defaults to true."
   :type 'boolean
   :group 'tokens)
 
-(defun tokens--regex-for-token (token-type)
-  "Return a regex for matching tokens of type TOKEN-TYPE."
-  (cdr (assoc token-type tokens-regex)))
-
-(defun tokens--token-list ()
-    "Return the list of tokens."
-  (mapcar 'car tokens-regex))
-
 (defun tokens-kill-ring-save (token-type &optional match-number)
   "Search the current line for the Nth MATCH-NUMBER token.
+
 The search is done for TOKEN-TYPE tokens.  When a match is found
-it is saved to the kill ring.  If MATCH-NUMBER is ommited it defaults
-to 1."
+it is saved to the kill ring.  If MATCH-NUMBER is ommited it
+defaults to 1."
   (interactive
    (list (intern (completing-read
                   "Enter token type: "
@@ -68,48 +62,58 @@ to 1."
   (save-excursion
     (beginning-of-line)
     (let ((match-number (or match-number 1)))
-      (when (search-forward-regexp
-             (tokens--regex-for-token token-type)
-             (line-end-position)
-             t
-             match-number)
+      (when (tokens--search token-type match-number)
         (tokens--save-to-kill-ring (match-beginning 0) (match-end 0))))))
 
 (defun tokens-kill-ring-save-any (&optional match-number)
   "Search the current line for the Nth (MATCH-NUMBER) token.
+
 The search is done for any type of token.  When a match is found
-it is saved to the kill ring.  If MATCH-NUMBER is ommited it defaults
-to 1."
+it is saved to the kill ring.  If MATCH-NUMBER is ommited it
+defaults to 1."
   (interactive "P")
 
   (save-excursion
     (let ((match-number (or match-number 1)))
       (catch 'found
         (dolist (token-type (tokens--token-list))
-          (message "Searching for %s" (tokens--regex-for-token token-type))
-          (beginning-of-line)
-
-          (when (search-forward-regexp
-                 (tokens--regex-for-token token-type)
-                 (line-end-position)
-                 t
-                 match-number)
-            (tokens--save-to-kill-ring
-             (match-beginning 0) (match-end 0))
+          (when (tokens-kill-ring-save token-type match-number)
             (throw 'found (match-string 0))))))))
 
+(defun tokens--token-list ()
+    "Return the list of tokens."
+  (mapcar 'car tokens-regex))
+
+(defun tokens--regex-for-token (token-type)
+  "Return a regex for matching tokens of type TOKEN-TYPE."
+  (cdr (assoc token-type tokens-regex)))
+
+(defun tokens--search (token-type match-number)
+  "Search the current line for the Nth MATCH-NUMBER of TOKEN-TYPE."
+  (search-forward-regexp
+   (tokens--regex-for-token token-type)
+   (line-end-position)
+   t
+   match-number))
+
 (defun tokens--save-to-kill-ring (region-start region-end)
-  "Save the region between REGION-START and REGION-END to the kill ring."
+  "Save the region between REGION-START and REGION-END to the kill ring.
+
+If 'tokens-highlight-match' is non-nil and `pulse' is available,
+the saved region is highlight.  Also, if `tokens-verbose-match' is
+non-nil, the saved region is shown on the echo area.
+
+Return saved region as a string."
   (kill-ring-save region-start region-end)
 
   (when (and (eq tokens-highlight-match t)
              (functionp 'pulse-momentary-highlight-region))
     (pulse-momentary-highlight-region region-start region-end))
 
-  (when (eq tokens-verbose-match t)
-    (message "Saved: %s"
-             (buffer-substring-no-properties region-start region-end))))
+  (let ((token (buffer-substring-no-properties region-start region-end)))
+    (when (eq tokens-verbose-match t)
+      (message "Saved: %s"token))
+    token))
 
-  
 (provide 'tokens)
 ;;; tokens.el ends here
